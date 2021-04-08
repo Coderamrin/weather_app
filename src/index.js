a530fecf4808d4da253ef0b2ae81383f072da79b
amr1n <amrin0737@gmail.com>
2021-04-08 19:27:57 +0600

    completed the design

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ function getWeather(location, apikey ){
         return res.json(); 
     })
     .then(function(r) {
-        dom.temp.textContent = "Temperature:" + r.main.temp + " C";
-        dom.humidity.textContent = "Humidity:" + r.main.humidity + " %";
-        dom.wind.textContent = "Wind:" + r.wind.speed + " km/h";
-    });
-}
+        dom.temp.textContent = r.main.temp + " C";
+        dom.humidity.textContent = r.main.humidity + " %";
+        dom.wind.textContent = r.wind.speed + " km/h";
+    }); 
+} 
 
 let dom = (function () {
+    let displayWeather = document.querySelector(".displayWeather");
     let cityname = document.querySelector(".cityName");
     let search = document.querySelector(".search");
     let temp = document.querySelector(".temp");
@@ -22,13 +23,21 @@ let dom = (function () {
         temp,
         humidity,
         wind,
+        displayWeather,
     }
 })();
 
 let apikey="2aa31b1d167d0f8341bd55e8c2aba6ae"; 
 function searchCb() {
+    if(dom.cityname.value == "") {
+        return
+    }
+
     let location = dom.cityname.value;
     getWeather(location, apikey);
+    dom.displayWeather.classList.remove("hide"); 
+    dom.cityname.value = "";
+    dom.cityname.placeholder = "enter city name"
 }
 
 dom.search.addEventListener("click", searchCb);
